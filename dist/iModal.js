0976f68979f1e4578abb1afe5d9fc32092cf9e05
杨帆 <hzyang_fan@corp.netease.com>
2015-05-22 17:17:47 +0800

    add object.keys

--- dist/iModal.js
+++ dist/iModal.js
@@ -92,5 +92,38 @@
         };
     }
 
+    // The Object.keys() method returns an array of a given object's own enumerable properties.
+    if (!Object.keys) {
+        Object.keys = (function () {
+            var hasOwnProperty = Object.prototype.hasOwnProperty,
+                hasDontEnumBug = !({toString: null}).propertyIsEnumerable('toString'),
+                dontEnums = [
+                    'toString',
+                    'toLocaleString',
+                    'valueOf',
+                    'hasOwnProperty',
+                    'isPrototypeOf',
+                    'propertyIsEnumerable',
+                    'constructor'
+                ],
+                dontEnumsLength = dontEnums.length;
+
+            return function (obj) {
+                if (typeof obj !== 'object' && !$t.$isFunction(obj) || obj === null) return;
+
+                var result = [];
+                for (var prop in obj) {
+                    if (hasOwnProperty.call(obj, prop)) result.push(prop);
+                }
+                if (hasDontEnumBug) {
+                    for (var i = 0; i < dontEnumsLength; i++) {
+                        if (hasOwnProperty.call(obj, dontEnums[i])) result.push(dontEnums[i]);
+                    }
+                }
+                return result;
+            }
+        })()
+    }
+
     if (!_win.$m) _win.$m = $m;
 })(document, window)
