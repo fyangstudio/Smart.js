00ae0f2dc7c0fdd11fad229ea76e4fe7bb440b97
杨帆 <hzyang_fan@corp.netease.com>
2015-05-29 11:44:52 +0800

    add base class

--- test/javascript/iModal.js
+++ test/javascript/iModal.js
@@ -484,9 +484,85 @@
      * Define (SAMD)
      *
      */
+    // The base Class implementation
+    var Class = function () {
+    };
+
+    // Create a new Class that inherits from this class
+    Class.$extend = function (prop) {
+        var _super = this.prototype;
+
+        // Instantiate a base class (but only create the instance,
+        // don't run the init constructor)
+        var prototype = new this();
+
+        // Copy the properties over onto the new prototype
+        for (var name in prop) {
+            if (name != "$super") {
+                // Check if we're overwriting an existing function
+                prototype[name] = ($t.$isFunction(prop[name]) && $t.$isFunction(_super[name]) ?
+                    (function (name, fn) {
+                        return function () {
+                            var tmp = this.$super;
+
+                            // Add a new ._super() method that is the same method
+                            // but on the super-class
+                            this.$super = _super[name];
+
+                            // The method only need to be bound temporarily, so we
+                            // remove it when we're done executing
+                            var ret = fn.apply(this, arguments);
+                            this.$super = tmp;
+
+                            return ret;
+                        };
+                    })(name, prop[name]) : prop[name]);
+            }
+        }
+
+        // The dummy class constructor
+        function _Class() {
+        }
+
+        for (var key in this) {
+            if (this.hasOwnProperty(key) && key != "$extend")
+                _Class[key] = this[key];
+        }
+
+        // Populate our constructed prototype object
+        _Class.prototype = prototype;
+
+        // Enforce the constructor to be what we expect
+        _Class.prototype.constructor = _Class;
+
+        // And make this class extendable
+        _Class.$extend = Class.$extend;
+
+        return _Class;
+    };
+
+    var x = Class.$extend({
+        $init: function () {
+            console.log(1);
+        },
+        t1: function () {
+        }
+    })
+
+    var t = x.$extend({
+        $init: function () {
+            this.$super();
+            console.log(2);
+        },
+        t2: function () {
+            this.$super();
+        }
+    })
+    new t().$init();
+
     var $m = {
         config: function (options) {
-            console.log(options);
+            // console.log(options);
         }
     }
 
