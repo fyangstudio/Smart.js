7098ca5a507b304c0fbe50073eaffc50ba2f1f23
杨帆 <hzyang_fan@corp.netease.com>
2015-06-26 11:20:26 +0800

    fix map parse

--- test/javascript/lib/iModal/iModal-0.1.0.js
+++ test/javascript/lib/iModal/iModal-0.1.0.js
@@ -1021,7 +1021,7 @@
      * Living dom
      *
      */
-
+    // Credit: regularjs 0.2.15-alpha (c) leeluolee <http://regularjs.github.io> MIT License
     var _macro = {
         'BEGIN': '{{',
         'END': '}}',
@@ -1039,24 +1039,18 @@
         }, 'TAG']
     };
 
-    function wrapHander(handler) {
-        return function (all) {
-            return {type: handler, value: all}
-        }
-    }
-
     var _processRules = function (rules) {
         var rule, map = {}, sign;
-        for (var i = 0, len = rules.length; i < len; i++) {
-            rule = rules[i];
+        rules.forEach(function (value) {
+            rule = value;
             sign = rule[2] || 'INIT';
             (map[sign] || (map[sign] = {rules: [], links: []})).rules.push(rule);
-        }
+        });
         return setup(map);
     };
 
     function setup(map) {
-        var split, rules, trunks, handler, reg, retain, rule;
+        var split, rules, _matchs, reg, rule;
 
         var _replaceFn = function ($, one) {
             return $m.$isString(_macro[one]) ? $m.$escapeRegExp(_macro[one]) : String(_macro[one]).slice(1, -1);
@@ -1067,28 +1061,22 @@
             split = map[i];
             split.curIndex = 1;
             rules = split.rules;
-            trunks = [];
+            _matchs = [];
 
             for (var j = 0, len = rules.length; j < len; j++) {
                 rule = rules[j];
                 reg = rule[0];
-                handler = rule[1];
-
-                if ($m.$isString(handler)) {
-                    handler = wrapHander(handler);
-                }
                 if ($m.$isRegExp(reg)) reg = reg.toString().slice(1, -1);
 
                 reg = reg.replace(/%(\w+)%/g, _replaceFn);
-                trunks.push(reg);
+                _matchs.push(reg);
             }
-            split.TRUNK = new RegExp("^(?:(" + trunks.join(")|(") + "))")
+            split.MATCH = new RegExp("^(?:(" + _matchs.join(")|(") + "))")
         }
         return map;
     }
 
     var map1 = _processRules([
-
         //TAG
         _rules.TAG_OPEN,
         _rules.TAG_CLOSE
