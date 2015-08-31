d2614c76a6f14640a678c1603c5ded01ae3b12bc
杨帆 <hzyang_fan@corp.netease.com>
2015-08-31 10:12:48 +0800

    add test

--- test/javascript/lib/iModal/iModal-1.0.0.js
+++ test/javascript/lib/iModal/iModal-1.0.0.js
@@ -1707,11 +1707,14 @@
             sign1 = '_dom' + (this.sign++) + '_',
             holder = '',
             ret = 'var ' + sign1 + '=' + '$m.$create("' + statement.NAME + '");';
+        var test = $m.$create(statement.NAME);
         if (attrs.length) {
             attrs.forEach(function (value) {
                 ret += ('$m.$attr(' + sign1 + ',"' + value.NAME + '","' + value.VALUE + '");');
+                $m.$attr(test, value.NAME, value.VALUE);
             });
         }
+        console.log(test);
         if (statement.CHILDREN.length) {
             body = this.process(statement.CHILDREN, sign1);
             ret += body.piece;
